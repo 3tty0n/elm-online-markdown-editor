b6e50cc6f9f49444125fba82b4f1d0abd04d1f94
YuIzawa <yuizalp@gmail.com>
2016-12-01 18:53:00 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 git branch -D gh-pages
 git checkout -b gh-pages
 make
-git add -p .
+git add index.html Markdown.elm style.css elm-package.json
 git add -f elm.js
+git commit -m "deploy"
 git push -f origin gh-pages
